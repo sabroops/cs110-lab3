a731bdb23a1d355175c9fe6bf8196fff7a13dff5
sabroops <scarl011@ucr.edu>
2021-05-05 23:28:44 -0700

    comments
    Co-authored-by: achen163 <achen163@users.noreply.github.com>

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function pauseFunction(){
 function searchTweets() {
     var input = document.getElementById('searchbar') //searchBar
     var filter = input.value.toLowerCase();
-    var tweets = document.getElementsByClassName('content-middle') //tweets flexTweet
+    var tweets = document.getElementsByClassName('content-middle') //tweets container from html content-middle
 
     for(var i = 0; i < tweets.length; i++){
         var a = tweets[i].getElementsByClassName('post-content')[0]; // tweetText
@@ -56,16 +56,11 @@ function searchTweets() {
     }
 }
 
-function URLError(url){
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    return (http.status != 404);
-}
+
+
 
 var id = [];
 const tweetContainer = document.getElementsByClassName("content-middle-wrapper"); //centerfeed
-var flip = [];
 var tweetArr = [];
 
 
@@ -84,43 +79,41 @@ function refreshTweets(tweets) {
 
     // For each tweet we get, put it in a larger div tag
     tweetStatus.forEach((tweet) => {
-        // Do not display duplicates
+        // This checks duplicates
         if(!id.includes(tweet.id_str)){
             id.push(tweet.id_str);
-            // Create overall div for tweet
-            var tweetElement = document.createElement("div");
-            tweetElement.className = 'tweets';
-            var profilePictureDiv = document.createElement("div");
+
+            // var's used
+            var tweetElement = document.createElement("div");           //div for the whole element
+            var profilePictureDiv = document.createElement("div");       //div for profile pic
+            var tweetBox = document.createElement('div');                  //div for the tweet box
+            var name = document.createElement('span');                          //span for name 
+            var screenName = document.createElement('div');                     //screen name will have both name + date
+            var tweetDate = tweet.user.created_at.slice(4,10);                      //creating date var
+            var tweetWords = document.createElement('p');                       //paragraph of the tweet
 
             //image element; FIXME*** some users dont have a pic
+            
             const profilePicture = document.createElement("img");
             profilePicture.src = tweet.user.profile_image_url;
-            profilePicture.className = "profilePicture";
             profilePictureDiv.append(profilePicture);
             profilePictureDiv.className = "profile-middle";
             tweetElement.append(profilePictureDiv);
 
-            //USERNAME
-            var tweetBox = document.createElement('div');
-            var name = document.createElement('span');
+            //GET USERNAME append to our div
+           
             name.appendChild(document.createTextNode(tweet.user.name));
-            name.className = 'username post-name';
-            tweetBox.className = 'post-box'; 
             tweetBox.append(name);
 
-            //  Make  profile date span
-            var screenName = document.createElement('div');
-            var tweetDate = tweet.user.created_at.slice(4,10)
+            //  Make  profile date span adding to username
+          
             screenName.appendChild(document.createTextNode(' @' + tweet.user.screen_name + ' ' + tweetDate + ' '));
-            screenName.className = 'screenName';
-            tweetBox.className = 'tweetBoxContainer';
             tweetBox.append(screenName);
 
             // TEXT
-            var tweetWords = document.createElement('p');
+            
             tweetWords.appendChild(document.createTextNode(tweet.text));
             tweetWords.className = 'post-content';
-            tweetBox.className = 'tweetBoxContainer';
             tweetBox.append(tweetWords);
             tweetElement.append(tweetBox);
             tweetElement.className = 'content-middle'; 
@@ -129,11 +122,9 @@ function refreshTweets(tweets) {
     });
 
     // Sort by date
-
-
-
-
-    //putting children into the one big array that will keep adding onto 
+    
+    
+    //putting children into the array 
     for(var i = (tweetArr.length-1); i >= 0; i--){
         tweetList.appendChild(tweetArr[i]);
     }
